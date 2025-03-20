1f51dfeae2343a1230d0996c428a4d69b2a4af1f
lakshaysharma99 <lakshays206@gmail.com>
2025-03-20 18:35:59 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Application features
+ 
+- User can track mood - although on day basis for now. Could not get time to complete weekly and monthly timeline.
+- User can view mood name by hovering over the mood
+
+# Screenshots
+![alt text](image.png)
+
+# Deployment link
+
+
+## Note
+I know that I could not complete this project fully. I got busy in my company work. So I could not complete.
--- script.js
+++ script.js
@@ -3,99 +3,48 @@
 const moodOptions = document.querySelectorAll('.mood-option')
 
 //get moodRecords if available in localstorage or declare as empty object
-let moodRecords = JSON.parse(localStorage.getItem("moodRecords")) || {}
+let moodRecords = JSON.parse(localStorage.getItem("moodRecords")) || []
 
 //add event listener on each mood
 moodOptions.forEach((mood) => {
     mood.addEventListener("click", (e) => {
         const selectedMood = mood.innerHTML
         const currentDate = new Date()
-        console.log(typeof currentDate)
 
-        moodRecords[currentDate] = selectedMood
+        const moodLog = { date: currentDate, moodValue: selectedMood}
+
+        moodRecords.push(moodLog)
         localStorage.setItem("moodRecords", JSON.stringify(moodRecords));
+        createMoodTimeline()
     })
 })
-
-// const timelineBtn = document.querySelectorAll(".timeline-btn")
-
-// timelineBtn.forEach((type) => {
-//     type.addEventListener("click", (e) => {
-//         createMoodTimeline(type.innerHTML)
-//     })
-// })
-
-
 const dayTimeline = document.getElementById("day-timeline")
 
-for (const mood in moodRecords) {
-    const moodRow = document.createElement("div")
-    
-    const moodTime = document.createElement("span")
-    moodTime.innerHTML = new Date(mood).toTimeString().split(' ')[0]
-    
-    const moodValue = document.createElement("span")
-    moodValue.innerHTML = moodRecords[mood]
-    
-    moodRow.appendChild(moodTime)
-    moodRow.appendChild(moodValue)
-    dayTimeline.appendChild(moodRow)
-}  
+function createMoodTimeline() {
+    dayTimeline.innerHTML = ''
 
-const dayBtn = document.getElementById("day-btn")
+    moodRecords.forEach((row) => {
+        const moodRow = document.createElement("div")
+        moodRow.classList.add('mood-row')
+        
+        const moodTime = document.createElement("span")
+        moodTime.innerHTML = new Date(row.date).toTimeString().split(' ')[0]
+        
+        const moodValue = document.createElement("span")
+        moodValue.innerHTML = row.moodValue
+        
+        moodRow.appendChild(moodTime)
+        moodRow.appendChild(moodValue)
+        dayTimeline.appendChild(moodRow)
+    })
+}
 
-dayBtn.addEventListener("click", (e) => {
-    dayTimeline.classList.remove("hidden")
-    weekTimeline.classList.add("hidden")
-    monthTimeline.classList.add("hidden")
+const resetBtn = document.getElementById("reset-timeline")
+resetBtn.addEventListener("click", (e) => {
+    localStorage.clear()
+    moodRecords = []
+    dayTimeline.innerHTML = ''
+
 })
 
-
-const weekTimeline = document.getElementById("week-timeline")
-
-for (const mood in moodRecords) {
-    const moodRow = document.createElement("div")
-    
-    const moodTime = document.createElement("span")
-    moodTime.innerHTML = new Date(mood).toTimeString().split(' ')[0]
-    
-    const moodValue = document.createElement("span")
-    moodValue.innerHTML = moodRecords[mood]
-    
-    moodRow.appendChild(moodTime)
-    moodRow.appendChild(moodValue)
-    weekTimeline.appendChild(moodRow)
-}  
-
-const weekBtn = document.getElementById("week-btn")
-
-weekBtn.addEventListener("click", (e) => {
-    dayTimeline.classList.add("hidden")
-    weekTimeline.classList.remove("hidden")
-    monthTimeline.classList.add("hidden")
-})
-
-
-const monthTimeline = document.getElementById("month-timeline")
-
-for (const mood in moodRecords) {
-    const moodRow = document.createElement("div")
-    
-    const moodTime = document.createElement("span")
-    moodTime.innerHTML = new Date(mood).toTimeString().split(' ')[0]
-    
-    const moodValue = document.createElement("span")
-    moodValue.innerHTML = moodRecords[mood]
-    
-    moodRow.appendChild(moodTime)
-    moodRow.appendChild(moodValue)
-    monthTimeline.appendChild(moodRow)
-}  
-
-const monthBtn = document.getElementById("month-btn")
-
-monthBtn.addEventListener("click", (e) => {
-    dayTimeline.classList.add("hidden")
-    weekTimeline.classList.add("hidden")
-    monthTimeline.classList.remove("hidden")
-})
+createMoodTimeline()
